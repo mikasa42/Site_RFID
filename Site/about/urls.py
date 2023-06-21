0da2659e6c53b20680807eed46d8e59a7be2bb3b
mikasa42 <mikasa201642@gmail.com>
2023-06-21 01:20:12 -0300

    Consertando o bug no menu.

--- Site/about/urls.py
+++ Site/about/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('helloword/', views.helloword),
     path('', views.paginainicial, name='index'),
+    path('historia/', views.historia),
 ]
--- Site/about/views.py
+++ Site/about/views.py
@@ -8,3 +8,6 @@ def helloword(request):
 
 def paginainicial(request):
     return render(request, 'home/index.html')
+
+def historia(request):
+    return render(request, 'home/historia.html')
