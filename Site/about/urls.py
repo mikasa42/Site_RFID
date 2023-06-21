ed237eca418d93f499cfc9a2f523e4e2451118e4
mikasa42 <mikasa201642@gmail.com>
2023-06-21 01:43:48 -0300

    Inserindo informações nas páginas

--- Site/about/urls.py
+++ Site/about/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     path('helloword/', views.helloword),
     path('', views.paginainicial, name='index'),
     path('historia/', views.historia),
+    path('aplicacoes/', views.aplicacoes),
+    path('funcionamento/', views.funcionamento),
+    path('objetivo/', views.objetivo),
+    path('problemas/', views.problemas),
+    path('Tipos/', views.Tipos),
 ]
--- Site/about/views.py
+++ Site/about/views.py
@@ -11,3 +11,14 @@ def paginainicial(request):
 
 def historia(request):
     return render(request, 'home/historia.html')
+
+def aplicacoes(request):
+    return render(request, 'home/aplicacoes.html')
+def funcionamento(request):
+    return render(request, 'home/funcionamento.html')
+def objetivo(request):
+    return render(request, 'home/objetivo.html')
+def problemas(request):
+    return render(request, 'home/problemas.html')
+def Tipos(request):
+    return render(request, 'home/Tipos.html')
